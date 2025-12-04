d08e61e03ecbb99a0f9b588fddceabed1a8c732e
honzhayehor <honzhayehor@gmail.com>
2025-12-04 21:43:12 +0200

    feat(root class): Added necessary methods to a class

--- src/main/java/entities/Entity.java
+++ src/main/java/entities/Entity.java
@@ -3,10 +3,25 @@ package entities;
 import logic.Coordinates;
 
 public abstract class Entity {
-    private boolean walkable;
-    public Coordinates coordinates;
+    private final boolean walkable;
+    private Coordinates coordinates;
 
+    public Entity(boolean walkable, Coordinates coordinates) {
+        this.walkable = walkable;
+        this.coordinates = coordinates;
+    }
 
-    public abstract void setCoordinates();
-    public boolean blocksMovement() {return this.walkable;}
+    public abstract String getAvatar();
+
+    public boolean blocksMovement() {
+        return !walkable;
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+        this.coordinates = coordinates;
+    }
 }
