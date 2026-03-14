a1f3ea793b08e55a12cfacf18b7b7a4039413c6e
Honzha Yehor <honzhayehor@gmail.com>
2026-03-14 19:46:07 +0200

    refactor: Interface for path finding algorithm added
    Added new algorithm that all adapters of Graph algorithms must implement to allow Creature to use it. Added proper constructors to match Creature signature

--- src/main/java/units/abstraction/Predator.java
+++ src/main/java/units/abstraction/Predator.java
@@ -1,12 +1,12 @@
 package units.abstraction;
 
 import enviroment.WorldMap;
-import logic.Pathfinder;
+import logic.AStar;
 import units.configs.SightRange;
 
 public abstract class Predator extends Creature{
 
-    protected Predator(WorldMap map, Pathfinder algorithm, SightRange range, int baseHp, int moveSpeed) {
+    protected Predator(WorldMap map, AStar algorithm, SightRange range, int baseHp, int moveSpeed) {
         super(map, algorithm, range, baseHp, moveSpeed);
     }
 }
