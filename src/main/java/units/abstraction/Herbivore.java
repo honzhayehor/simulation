2af13627e0e49a1cbf59964949046de5a16fd554
Honzha Yehor <honzhayehor@gmail.com>
2026-03-16 20:27:52 +0200

    fix: Fixed constructor to match super class

--- src/main/java/units/abstraction/Herbivore.java
+++ src/main/java/units/abstraction/Herbivore.java
@@ -2,14 +2,11 @@ package units.abstraction;
 
 import enviroment.WorldMap;
 import logic.AStar;
-import units.configs.BaseHp;
-import units.configs.CreatureAttackPower;
-import units.configs.CreatureMoveSpeed;
-import units.configs.SightRange;
+import units.configs.*;
 import units.interfaces.MeatBased;
 
 public abstract class Herbivore extends Creature implements MeatBased {
-    protected Herbivore(WorldMap map, AStar algorithm, SightRange range, BaseHp baseHp, CreatureMoveSpeed creatureMoveSpeed, CreatureAttackPower attackPower) {
-        super(map, algorithm, range, baseHp, creatureMoveSpeed, attackPower);
+    protected Herbivore(WorldMap map, AStar algorithm, SightRange range, BaseHp baseHp, CreatureMoveSpeed creatureMoveSpeed, CreatureAttackPower attackPower, StarvationRate starvationRate) {
+        super(map, algorithm, range, baseHp, creatureMoveSpeed, attackPower, starvationRate);
     }
 }
--- src/main/java/units/abstraction/Predator.java
+++ src/main/java/units/abstraction/Predator.java
@@ -2,14 +2,11 @@ package units.abstraction;
 
 import enviroment.WorldMap;
 import logic.AStar;
-import units.configs.BaseHp;
-import units.configs.CreatureAttackPower;
-import units.configs.CreatureMoveSpeed;
-import units.configs.SightRange;
+import units.configs.*;
 
 public abstract class Predator extends Creature{
-    protected Predator(WorldMap map, AStar algorithm, SightRange range, BaseHp baseHp, CreatureMoveSpeed creatureMoveSpeed, CreatureAttackPower attackPower) {
-        super(map, algorithm, range, baseHp, creatureMoveSpeed, attackPower);
+    protected Predator(WorldMap map, AStar algorithm, SightRange range, BaseHp baseHp, CreatureMoveSpeed creatureMoveSpeed, CreatureAttackPower attackPower, StarvationRate starvationRate) {
+        super(map, algorithm, range, baseHp, creatureMoveSpeed, attackPower, starvationRate);
     }
 
 
